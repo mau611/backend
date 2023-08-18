5b0bf33dca2e1f4ee901572ce22e21788d0f2f2c
mau611 <mauricio611rp@gmail.com>
2023-08-18 11:21:45 -0400

    login working

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
@@ -12,10 +14,18 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
+    protected function redirectTo($request): ?string {
+        return $request->expectsJson() ? null : route('login');
+    }
+
+    public function handle($request, Closure $next, ...$guards) {
+
+        if ($token = $request->cookie('token')) {
+            $request->headers->set('Authorization', 'Bearer ' . $token);
         }
+
+        $this->authenticate($request, $guards);
+
+        return $next($request);
     }
 }
